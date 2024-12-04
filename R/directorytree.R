12f9efb25dcf22227356c0254de06da3e9689de7
MMolBus <manuel.molbus@gmail.com>
2024-12-04 11:39:57 +0100

    Refactor directorytree function for improved readability and cross-platform compatibility
    - Renamed parameter `only.directories` to `only_directories` for consistency with R naming conventions.
    - Replaced logical values `T` and `F` with `TRUE` and `FALSE` for clarity.
    - Utilized `.Platform$file.sep` to handle path separators across different operating systems.
    - Enhanced overall readability and robustness of the function.

--- R/directorytree.R
+++ R/directorytree.R
@@ -12,50 +12,43 @@
 #' path.
 #'
 #' @param path A character string specifying the root directory path.
-#' @param only.directories A logical value indicating whether to include only 
+#' @param only_directories A logical value indicating whether to include only 
 #' directories (TRUE) or both files and directories (FALSE). Default is TRUE.
 #' @return A Node object representing the directory tree.
 #' @examples
 #' \dontrun{
-#' tree <- directory.tree("/path/to/directory", only.directories = TRUE)
+#' tree <- directorytree("/path/to/directory", only_directories = TRUE)
 #' print(tree)
 #' }
 #' @export
-# function ----------------------------------------------------------------
-directorytree <- function(path, only.directories = T) {
+directorytree <- function(path, only_directories = TRUE) {
   
   # Create a new Node object with the base name of the provided path
   root <- data.tree::Node$new(basename(path))
-
   
   # Check if only directories should be listed
-  if(only.directories == T){
-    
+  if (only_directories) {
     # Get a list of all directories within the specified path
     tips <- list.dirs(path, recursive = TRUE, full.names = TRUE)
   } else {
-    
     # Get a list of all files and directories within the specified path
     tips <- list.files(path, recursive = TRUE, full.names = TRUE)
   }
   
   # Iterate over each item in the list
   for (tip in tips) {
-    
     # Split the path into its components
-    parts <- strsplit(tip, "/")[[1]]
+    parts <- strsplit(tip, .Platform$file.sep)[[1]]
     
     # Start from the root node
     actual_node <- root
     
     # Iterate over each part of the path
     for (part in parts) {
-      
       # If the part is not already a child of the current node, add it
       if (!part %in% names(actual_node$children)) {
         actual_node$AddChild(part)
       }
-      
       # Move to the child node
       actual_node <- actual_node$children[[part]]
     }
